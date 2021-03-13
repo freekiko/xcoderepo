7b556a11e44c2fc2c763a40f4d31ca356de92ecf
Lucas QUEIROS <lucasqueiros@outlook.fr>
2021-03-13 14:27:39 +0100

    1st commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Lucas on 13/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("digi!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/kokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kokoApp.swift
+//  Shared
+//
+//  Created by Lucas on 13/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct kokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
